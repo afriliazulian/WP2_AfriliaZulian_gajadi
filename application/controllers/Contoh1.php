a18c59e01e2ff708477f2ed7c70f0faf7a12e148
afriliazulian <afrilia.zulian@gmail.com>
2023-09-26 20:56:40 +0700

    Update Contoh1

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama Saya Afrilia Zulian
+        Saya tinggal di Wisma Asri 2
+        olahraga yang saya sukai adalah basket";
+    }
+}
